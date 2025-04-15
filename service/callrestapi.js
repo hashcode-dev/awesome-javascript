248526d79f5085d1c6b105ceed714d883699df9e
Sahu, Gaurav <hashcode.dev@gmail.com>
2025-04-15 21:00:38 +0530

    [XXX-0000]: Sixth JavaScript Commit

--- service/callrestapi.js
+++ service/callrestapi.js
@@ -1,9 +1,11 @@
 
 
 function callRestApi() {
+    spinner.style.display = 'block'; // Show spinner
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
         .then(data => {
+            spinner.style.display = 'none'; // Hide spinner
             console.log('Data:', data);
             document.getElementById('demo').innerHTML = data.title;
         })
